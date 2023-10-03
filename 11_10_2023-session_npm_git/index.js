6ff99066c8fa25ea3688c2f075bf5584b9ac5775
wildmatthieu <matthieu.lopez@wildcodeschool.com>
2023-10-03 11:59:48 +0200

    up avec oubli gitignore sorry bro

--- 11_10_2023-session_npm_git/index.js
+++ 11_10_2023-session_npm_git/index.js
@@ -2,6 +2,6 @@ import chalk from 'chalk';
 import { input } from '@inquirer/prompts';
 
 console.log(chalk.red('Hello world!'));
-const answer = input({ message: 'Enter your name' });
+const toto = input({ message: 'Enter your name' });
 
-console.log(answer);
+console.log(toto);
